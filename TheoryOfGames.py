8a6a30e6a9998abe720ba9ca03fd077ffee04e0c
nik <borecnick@mail.ru>
2017-11-21 20:56:20 +0300

    Lab1 ends

--- TheoryOfGames.py
+++ TheoryOfGames.py
@@ -70,14 +70,14 @@ def simplex(a, n, m):
     m_min, m_ind = get_resh_col(a, n, m)
     s_min, s_ind = get_resh_row(a, n, m_ind)
     a1 = get_new_table(a, n, m, m_ind, s_ind)
-    return a1
+    return a1, m_ind, s_ind
 
 
 def simplex2(a, n, m):
     s_min, s_ind = get_resh_row2(a, n)
     m_min, m_ind = get_resh_col2(a, m, s_ind)
     a1 = get_new_table(a, n, m, m_ind, s_ind)
-    return a1
+    return a1, m_ind, s_ind
 
 
 # aa = Matrix([
@@ -90,35 +90,55 @@ aa = Matrix([
    [8, 4, 1, 0, 1],
    [0, -8, -6, 0, 0]
 ])
-xx = [1, 2]
+xx = [3, 4]
 pprint(aa)
-a2 = simplex(aa, 3, 5)
+m_ind=-1
+s_ind=-1
+a2, m_ind, s_ind = simplex(aa, 3, 5)
+xx[s_ind] = m_ind
 pprint(a2)
-a3 = simplex(a2, 3, 5)
+print(xx)
+a3, m_ind, s_ind = simplex(a2, 3, 5)
+
+xx[s_ind] = m_ind
 pprint(a3)
 a3 = a3.col_insert(5, Matrix([0, 0, 0]))
 a3 = a3.row_insert(2, Matrix([[-Integer(12)/Integer(17), 0, 0, -Integer(16)/Integer(17), -Integer(5)/Integer(17), 1]]))
+xx.append(1)
 pprint(a3)
-a4 = simplex2(a3, 4, 6)
+a4, m_ind, s_ind = simplex2(a3, 4, 6)
+xx[s_ind] = m_ind
 print("lol")
 pprint(a4)
 a4 = a4.col_insert(6, Matrix([0, 0, 0, 0]))
 a4 = a4.row_insert(3, Matrix([[-Integer(3)/Integer(4), 0, 0, 0, -Integer(5)/Integer(16), -Integer(15)/Integer(16), 1]]))
+xx.append(1)
 pprint(a4)
-a5 = simplex2(a4, 5, 7)
+a5, m_ind, s_ind = simplex2(a4, 5, 7)
+xx[s_ind] = m_ind
 pprint(a5)
 a5 = a5.col_insert(7, Matrix([0, 0, 0, 0, 0]))
 a5 = a5.row_insert(4, Matrix([[-Integer(4)/Integer(5), 0, 0, 0, -Integer(2)/Integer(3), 0, -Integer(4)/Integer(15), 1]]))
+xx.append(1)
 pprint(a5)
-a6 = simplex2(a5, 6, 8)
+a6, m_ind, s_ind = simplex2(a5, 6, 8)
+xx[s_ind] = m_ind
 pprint(a6)
 a6 = a6.col_insert(8, Matrix([0, 0, 0, 0, 0, 0]))
 a6 = a6.row_insert(5, Matrix([[-Integer(4)/Integer(5), 0, 0, 0, 0, 0, -Integer(3)/Integer(5), 0, 1]]))
+xx.append(1)
 pprint(a6)
-a7 = simplex2(a6, 7, 9)
+a7, m_ind, s_ind = simplex2(a6, 7, 9)
+xx[s_ind] = m_ind
 pprint(a7)
 a7 = a7.col_insert(9, Matrix([0, 0, 0, 0, 0, 0, 0]))
 a7 = a7.row_insert(6, Matrix([[-Integer(2)/Integer(3), 0, 0, 0, 0, 0, 0, -Integer(1)/Integer(2), -Integer(2)/Integer(3), 1]]))
+xx.append(1)
 pprint(a7)
-a8 = simplex2(a7, 8, 10)
+print(xx)
+a8, m_ind, s_ind = simplex2(a7, 8, 10)
+xx[s_ind] = m_ind
 pprint(a8)
+print(xx)
+for i in range(len(xx)):
+    print("x",xx[i],"=",a8[i,0])
